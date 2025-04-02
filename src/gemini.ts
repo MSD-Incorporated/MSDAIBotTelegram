296068b1a7d45767eaa649d6428e3dceb019bce8
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-04-02 23:19:26 +0500

    little changes

--- src/gemini.ts
+++ src/gemini.ts
@@ -9,6 +9,65 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_TOKEN);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const context: Record<number, { role: "model" | "user"; parts: { text: string }[] }[]> = {};
 
+gemini.hears("@masedmsd_ai_bot", async ctx => {
+	if (!userIDs.includes(ctx.from!.id)) return;
+
+	const args = ctx.msg.text!.split(/\s+/);
+	if (!args.length) return ctx.reply("Не удалось найти запрос...");
+
+	await ctx.replyWithChatAction("typing");
+	return ctx
+		.reply("Подождите, ответ генерируется...", {
+			reply_parameters: { message_id: ctx.msgId },
+			reply_markup: {
+				inline_keyboard: [[{ text: "Очистить контекст", callback_data: `clear_context_${ctx.from!.id}` }]],
+			},
+		})
+		.then(async ({ chat, message_id }) => {
+			if (!context[ctx.from!.id]) context[ctx.from!.id] = [];
+			const GPTchat = model.startChat({ history: context[ctx.from!.id] });
+
+			GPTchat.sendMessage(args.join(" ").replaceAll("@masedmsd_ai_bot", ""))
+				.then(async ({ response }) => {
+					const text = response.text();
+					const str = parser(text).slice(0, 4090);
+
+					context[ctx.from!.id]!.push({ role: "user", parts: [{ text: args.join(" ") }] });
+					context[ctx.from!.id]!.push({ role: "model", parts: [{ text }] });
+					context[ctx.from!.id]!.slice(context[ctx.from!.id]!.length - 10, context[ctx.from!.id]!.length);
+
+					return ctx.api
+						.editMessageText(chat.id, message_id, str, {
+							parse_mode: "HTML",
+						})
+						.catch(err => {
+							ctx.api.editMessageText(
+								chat.id,
+								message_id,
+								"Произошла неизвестная ошибка. Возможно потому что ответ нейросети был больше, чем лимиты на длину сообщения в Telegram"
+							);
+							console.error(err);
+						});
+				})
+				.catch(err => {
+					ctx.api.editMessageText(chat.id, message_id, "Произошла неизвестная ошибка", {});
+					console.error(err);
+				});
+		})
+		.catch(err => {
+			console.error(err);
+		});
+});
+
+gemini.command("clear_context", async ctx => {
+	const id = ctx.from?.id!;
+	7;
+
+	context[id] = [];
+	await ctx.answerCallbackQuery({ text: "Контекст успешно очищен!" });
+	await ctx.editMessageReplyMarkup({ reply_markup: { inline_keyboard: [] } });
+});
+
 gemini.command("gemini", async ctx => {
 	if (!userIDs.includes(ctx.from!.id)) return;
 
@@ -39,16 +98,6 @@ gemini.command("gemini", async ctx => {
 					return ctx.api
 						.editMessageText(chat.id, message_id, str, {
 							parse_mode: "HTML",
-							reply_markup: {
-								inline_keyboard: [
-									[
-										{
-											text: "Очистить контекст",
-											callback_data: `clear_context_${ctx.from!.id}`,
-										},
-									],
-								],
-							},
 						})
 						.catch(err => {
 							ctx.api.editMessageText(
@@ -60,18 +109,7 @@ gemini.command("gemini", async ctx => {
 						});
 				})
 				.catch(err => {
-					ctx.api.editMessageText(chat.id, message_id, "Произошла неизвестная ошибка", {
-						reply_markup: {
-							inline_keyboard: [
-								[
-									{
-										text: "Очистить контекст",
-										callback_data: `clear_context_${ctx.from!.id}`,
-									},
-								],
-							],
-						},
-					});
+					ctx.api.editMessageText(chat.id, message_id, "Произошла неизвестная ошибка", {});
 					console.error(err);
 				});
 		})
