8625225a107ea10116e3dd51b2b02ee517990e57
MasedMSD <68379695+MasedMSD@users.noreply.github.com>
2025-04-17 01:45:28 +0500

    fixed id

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import type { UserFromGetMe } from "grammy/types";
 
 export const userIDs: number[] = [654382771, 946070039, 1302930611] as const;
-export const channelID = -1002277829990 as const;
+export const channelID = -1002118873453 as const;
 
 export const onStart = ({ id, username }: UserFromGetMe) => console.log(`${username} [${id}] started!`);
 
